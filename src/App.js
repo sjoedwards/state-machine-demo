65dbb02e8d4ddbcc951cf560cffd94e177cd6ca8
sjoedwards <samuel.edwards@sky.uk>
2020-01-29 13:32:18 +0000

    Added correct CSS and design

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="pandas__hero">
-        <p className="pandas__hero__text">Adopt a panda today</p>
+      <div className="laceup__row">
+        <div className="laceup__grid-item">
+          <p className="laceup__banner__text">LACE UP</p>
+        </div>
+        <div className="laceup__grid-item">
+          <div className="laceup__search__container">
+            <div className="laceup__search__search-box">
+              <input type="text" placeholder="Search.." />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
