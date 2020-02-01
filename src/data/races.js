593094054a166170c3cb32fc08c915dc4fb8abea
sjoedwards <samuel.edwards@sky.uk>
2020-02-01 22:57:31 +0000

    Added race info

--- src/data/races.js
+++ src/data/races.js
@@ -17,32 +17,148 @@ const races = [
     }
   },
   {
-    title: 'Test Marathon',
+    title: 'Great North Run',
+    image:'/great-north-run.jpg',
     distance: {
-      short: '42.2K',
-      long: 'Full Marathon'
+      short: '21.1K',
+      long: 'Half Marathon'
     },
+    incline: {
+      description: 'Flat start, horrible hill around mile 10',
+      score: 6,
+    },
+    overall: {
+      description: 'Biggest in the world, hard to beat!',
+      score: 10,
+    }
   },
   {
-    title: 'Test Marathon',
+    title: 'Leeds Half Marathon',
+    image:'/leeds-half-marathon.jpg',
     distance: {
-      short: '42.2K',
-      long: 'Full Marathon'
+      short: '21.1K',
+      long: 'Half Marathon'
     },
+    incline: {
+      description: 'Up and down the whole way round, a killer on the quads!',
+      score: 9,
+    },
+    overall: {
+      description: 'A fairly decent course, lots of friendly people',
+      score: 7,
+    }
   },
   {
-    title: 'Test Marathon',
+    title: 'London Marathon',
+    image:'/london-marathon.jpg',
     distance: {
       short: '42.2K',
       long: 'Full Marathon'
     },
+    incline: {
+      description: 'Fast and Flat!',
+      score: 2,
+    },
+    overall: {
+      description: 'You can`t beat a finish on the Mall',
+      score: 9,
+    }
   },
   {
-    title: 'Test Marathon',
+    title: 'Leeds Abbey Dash',
+    image:'/leeds-abbey-dash.jpg',
+    distance: {
+      short: '10K',
+      long: 'Ten Kilometres'
+    },
+    incline: {
+      description: 'Relatively flat, good for a PB',
+      score: 3,
+    },
+    overall: {
+      description: 'A relatively uneventful route',
+      score: 7,
+    }
+  },
+  {
+    title: 'Paris Marathon',
+    image:'/paris-marathon',
     distance: {
       short: '42.2K',
       long: 'Full Marathon'
     },
+    incline: {
+      description: 'Relatively flat',
+      score: 4,
+    },
+    overall: {
+      description: 'Amazing scenery running along the Siene!',
+      score: 7,
+    }
+  },
+  {
+    title: 'York 10K',
+    image:'/york-10k.jpg',
+    distance: {
+      short: '10K',
+      long: '10 Kilometres'
+    },
+    incline: {
+      description: 'A few hills along the start, but relatively flat',
+      score: 4,
+    },
+    overall: {
+      description: 'Lovely scenery and a warm, friendly crowd',
+      score: 7,
+    }
+  },
+  {
+    title: '',
+    image:'/',
+    distance: {
+      short: '',
+      long: ''
+    },
+    incline: {
+      description: '',
+      score: 0,
+    },
+    overall: {
+      description: '',
+      score: 0,
+    }
+  },
+  {
+    title: '',
+    image:'/',
+    distance: {
+      short: '',
+      long: ''
+    },
+    incline: {
+      description: '',
+      score: 0,
+    },
+    overall: {
+      description: '',
+      score: 0,
+    }
+  },
+  {
+    title: '',
+    image:'/',
+    distance: {
+      short: '',
+      long: ''
+    },
+    incline: {
+      description: '',
+      score: 0,
+    },
+    overall: {
+      description: '',
+      score: 0,
+    }
   },
 ]
 
