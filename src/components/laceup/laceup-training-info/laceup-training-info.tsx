08638d3dcb6aa19f8541aa795fdf307ab4c61aa1
sjoedwards <samuel.edwards@sky.uk>
2020-04-13 19:06:48 +0100

    Training info

--- src/components/laceup/laceup-training-info/laceup-training-info.tsx
+++ src/components/laceup/laceup-training-info/laceup-training-info.tsx
@@ -7,19 +7,41 @@ interface TrainingInfoInterface {
 
 export default ({ current, send }: TrainingInfoInterface) => {
   const state = current.value;
-  return (<div className="machine__state-item__wrapper">
-    <div className="generic__row">
-      <div className="machine__state-item__wrapper">
-      </div>
-      <div className="machine__state-item__wrapper">
-      </div>
-      <div className="machine__state-item__wrapper">
-      </div>
-    </div>
-    <div className="generic__row">
-      <div className="generic__button generic__button__submit">
-        <button type="button" onClick={() => send('TEST')}>Continue</button>
-      </div>
-    </div>
+  const {matches} = current
+
+  const renderTrainingInfoPanel = () => {
+    if (matches('trainingInfo.races')) {
+      return (
+        <>
+          <div className="generic__row">
+            <p>How many races have you previously completed?</p>
+          </div>
+          <div className="generic__row">
+            <div className="generic__button generic__button__submit">
+              <button type="button" onClick={() => send('NUM_RACES_SUBMIT')}>Continue</button>
+            </div>
+          </div>
+        </>
+      );
+    }
+    if (matches('trainingInfo.kmPerWeek')) {
+      return (
+        <>
+          <div className="generic__row">
+            <p>How far do you train in total a week (km)?</p>
+          </div>
+          <div className="generic__row">
+            <div className="generic__button generic__button__submit">
+              <button type="button" onClick={() => send('NUM_RACES_SUBMIT')}>Continue</button>
+            </div>
+          </div>
+        </>
+      );
+    }
+  }
+
+  return (
+  <div className="machine__state-item__wrapper">
+      {renderTrainingInfoPanel()}
   </div>)
 };
